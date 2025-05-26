1867b106f0171f0726bc7c98402b6a864a710267
Vincent Porret <vincent@MacBook-Pro-de-Vincent.local>
2025-05-26 23:12:58 +0200

    origins

--- app/main.py
+++ app/main.py
@@ -3,6 +3,12 @@ from app.routers import balance, positions, orders, strategy, sync
 
 app = FastAPI()
 
+origins = [
+   "http://localhost:3000",  # React dev server
+    "http://localhost:5173",  # Vite dev server
+    "https://algo-project-e5b83.web.app"  # Add this if deploying your frontend
+]
+
 # Registering routers
 app.include_router(balance.router)
 app.include_router(positions.router)
