82db964484fa311ee27f9806f83db1390f79749d
Vincent Porret <vincent@MacBook-Pro-de-Vincent.local>
2025-05-26 23:27:11 +0200

    origins

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routers import balance, positions, orders, strategy, sync
+from fastapi.middleware.cors import CORSMiddleware 
+
 
 app = FastAPI()
 
@@ -8,6 +10,14 @@ origins = [
     "http://localhost:5173",  # Vite dev server
     "https://algo-project-e5b83.web.app"  # Add this if deploying your frontend
 ]
+
+app.add_middleware(
+   CORSMiddleware,
+      allow_origins=origins,
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
+   )
 
 # Registering routers
 app.include_router(balance.router)
